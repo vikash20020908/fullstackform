f310ac1341b0885dba0bbfad4edca7597e89a069
github_vikash20020908 <vikash20020908@gmail.com>
2023-09-16 12:14:38 +0530

    UPDATE

--- text utilities/src/Component/Navbar.js
+++ text utilities/src/Component/Navbar.js
@@ -1,19 +1,17 @@
 import React from 'react'
-
-
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg  navbar-dark bg-success  ">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/"> 🔴 Live </a>
+    <a className="navbar-brand" href="/"> TEXT UTILITIES </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/"> 🏠 Home</a>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">Link </a>
@@ -22,12 +20,7 @@ export default function Navbar() {
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Channel
           </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Location</a></li>
-            <li><a className="dropdown-item" href="/">Exclusive</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Refresh</a></li>
-          </ul>
+        
         </li>
         <li className="nav-item">
         </li>
